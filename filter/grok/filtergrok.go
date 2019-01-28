11575a3b6ca629a196e1df6301993f7874103e6f
tengattack <tengattack@foxmail.com>
2019-01-29 01:53:26 +0800

    filter/grok: update grok library

--- filter/grok/filtergrok.go
+++ filter/grok/filtergrok.go
@@ -3,10 +3,10 @@ package filtergrok
 import (
 	"context"
 
+	"github.com/tengattack/grok"
 	"github.com/tsaikd/gogstash/config"
 	"github.com/tsaikd/gogstash/config/goglog"
 	"github.com/tsaikd/gogstash/config/logevent"
-	"github.com/vjeantet/grok"
 )
 
 // ModuleName is the name used in config file
